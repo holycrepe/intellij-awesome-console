b7140e265c1452011bd9d9235f1a0cec16d2b7cc
Avinash Puchalapalli <git@puchalapalli.com>
2017-08-20 04:39:26 -0400

    Normalize Files for File Cache

--- src/awesome/console/AwesomeProjectFilesIterator.java
+++ src/awesome/console/AwesomeProjectFilesIterator.java
@@ -1,8 +1,11 @@
 package awesome.console;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +13,9 @@ import java.util.Map;
 public class AwesomeProjectFilesIterator implements ContentIterator {
 	private final Map<String, List<VirtualFile>> fileCache;
 	private final Map<String, List<VirtualFile>> fileBaseCache;
-
+    public static String normalizePath(String s) {
+        return s.replace("file://", "").replace( '/', File.separatorChar).replace('\\', File.separatorChar);
+    }
 	public AwesomeProjectFilesIterator(final Map<String, List<VirtualFile>> fileCache, final Map<String, List<VirtualFile>> fileBaseCache) {
 		this.fileCache = fileCache;
 		this.fileBaseCache = fileBaseCache;
@@ -19,8 +24,8 @@ public class AwesomeProjectFilesIterator implements ContentIterator {
 	@Override
 	public boolean processFile(final VirtualFile file) {
 		if (!file.isDirectory()) {
-			/** cache for full file name */
-			final String filename = file.getName();
+            /** cache for full file name */
+			final String filename = normalizePath(file.getName());
 			if (!fileCache.containsKey(filename)) {
 				fileCache.put(filename, new ArrayList<VirtualFile>());
 			}
